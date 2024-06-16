d296b8ff85b82a966e5a708fe8d19f6b9c13f618
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2024-06-16 15:12:11 -0300

    ADDED main function

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -2,9 +2,15 @@ package View;
 
 public class MainView implements View {
 	
+	public static void main(String[] args) {
+        MainView mainView = new MainView();
+
+        mainView.startView();
+    }
+	
 	@Override
 	public void startView() {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Library System!");
 
 	}
 
