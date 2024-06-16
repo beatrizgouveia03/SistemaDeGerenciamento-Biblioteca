4fb684321dfcd05a96238caf0441a4b8f3c6c770
Beatriz <72415136+beatrizgouveia03@users.noreply.github.com>
2024-06-16 12:37:33 -0300

    UPDATED Database Class

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -1,16 +1,17 @@
 package Database;
 
 import java.util.Map;
-import java.util.Optional;
-import java.util.HashMap;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Optional;
 
 import Entity.Entity;
 import Exception.DatabaseException;
 
 public class Database {
     /* Atributes */
-    Map<Class<? extends Entity>, DatabaseTableI<? extends Entity>> tables;
+    private Map<Class<? extends Entity>, DatabaseTableI<? extends Entity>> tables = new HashMap<>();
+    private static Database instance;
 
     /* Cosntructor */
     public Database() {
@@ -18,44 +19,55 @@ public class Database {
     }
 
     /* Methods */
-	public <T extends Entity> void save(Class<T> clazz, T entity) throws DatabaseException{
-        if (!tables.containsKey(clazz)) {
-            tables.put(clazz, new DatabaseTable<>());
+	public <T extends Entity> void save(Class<? extends Entity> class1, T entity) throws DatabaseException {
+        if (!tables.containsKey(class1)) {
+            tables.put(class1, new DatabaseTable<>());
         }
 
-        tables.get(clazz).save(entity);
+        DatabaseTableI<? extends Entity> table = tables.get(class1);
+        ((DatabaseTableI<T>) table).save(entity);
     }
-    <T extends Entity> Optional<? extends Entity> findById(Class<T> clazz, int id) throws DatabaseException{
+    public <T extends Entity> Optional<T> findById(Class<T> clazz, int id) throws DatabaseException{
         if(!tables.containsKey(clazz)){
             tables.put(clazz, new DatabaseTable<>());
         } 
 
-        return this.tables.get(clazz).findById(id);
+        DatabaseTableI<? extends Entity> table = tables.get(clazz);
+        return ((DatabaseTableI<T>) table).findById(id);
     }
 
-    <T extends Entity> List<? extends Entity> findAll(Class<T> clazz) throws DatabaseException{
+    public <T extends Entity> List<T> findAll(Class<T> clazz) throws DatabaseException{
         if(!tables.containsKey(clazz)){
             tables.put(clazz, new DatabaseTable<>());
         } 
 
-        return this.tables.get(clazz).findAll();
+        DatabaseTableI<? extends Entity> table = tables.get(clazz);
+        return ((DatabaseTableI<T>) table).findAll();
     }
 
-    <T extends Entity> void update(Class<T> clazz, int id, T entity) throws DatabaseException{
+    public <T extends Entity> void update(Class<T> clazz, int id, T entity) throws DatabaseException{
         if(!tables.containsKey(clazz)){
             tables.put(clazz, new DatabaseTable<>());
         } 
     
-        this.tables.get(clazz).update(id, entity);
+        DatabaseTableI<? extends Entity> table = tables.get(clazz);
+        ((DatabaseTableI<T>) table).update(id, entity);
     }
 
-    <T extends Entity> void delete(Class<T> clazz, int id) throws DatabaseException{
+    public <T extends Entity> void delete(Class<T> clazz, int id) throws DatabaseException{
         if(!tables.containsKey(clazz)){
             tables.put(clazz, new DatabaseTable<>());
         } 
         
-        this.tables.get(clazz).delete(id);
+        DatabaseTableI<? extends Entity> table = tables.get(clazz);
+        ((DatabaseTableI<T>) table).delete(id);
     }
-    
 
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+
+        return instance;
+    }
 }
